318d17d71dd78908ad553fed6a9417ed2d43d4e8
sanchopanda <ivanovaa1992@gmail.com>
2020-03-22 23:21:51 +0300

    form img

--- add.php
+++ add.php
@@ -1,8 +1,35 @@
 <?php require('functions.php');
 require('data.php');
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-$page_content = render('add.php', ['categories' => $categories]);
+
+    $required_fields = ['lot-name', 'message'];
+    $errors = [];
+    foreach ($required_fields as $field) {
+        if (empty($_POST[$field])) {
+            $errors[$field] = 'Поле не заполнено';
+        };
+    };
+
+    if (isset($_FILES['lot-image']['name'])) {
+        /* $tmp_name = $_FILES['lot-image']['tmp_name'];
+        $path = $_FILES['lot-image']['name'];
+
+        /*  $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $file_type = finfo_file($finfo, $tmp_name);*/
+
+        $path = 'проверка';
+        //move_uploaded_file($tmp_name, 'img/' . $path);
+    } else {
+        $errors['image'] = 'Вы не загрузили файл';
+    };
+
+    $page_content = render('add.php', ['path' => $path, 'errors' => $errors, 'lot_member' => $lot_member]);
+} else {
+    $page_content = render('add.php', []);
+};
+
 
 
 $layout_content = render(
--- templates/add.php
+++ templates/add.php
@@ -1,17 +1,23 @@
-<form class="form form--add-lot container form--invalid" action="add.php" method="post">
-    <!-- form--invalid -->
+<form class="form form--add-lot container <?php if (count($errors)) {
+                                                print('form-invalid');
+                                            }; ?>" action="add.php" method="post" enctype="multipart/form-data">
+
     <h2>Добавление лота</h2>
     <div class="form__container-two">
-        <div class="form__item form__item--invalid">
+        <div class="form__item <?php if ($errors['lot-name']) {
+                                    print('form__item--invalid');
+                                }; ?>">
             <!-- form__item--invalid -->
             <label for="lot-name">Наименование</label>
-            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
-            <span class="form__error">Введите наименование лота</span>
+            <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота"
+                value='<?= $lot_member['name']; ?>'>
+            <span class="form__error"><?php
+                                        print($errors['lot-name']);
+                                        ?></span>
         </div>
         <div class="form__item">
             <label for="category">Категория</label>
-            <select id="category" name="category" required>
-
+            <select id="category" name="category">
                 <?php $index = 0;
                 while ($index < count($categories)) : ?>
                 <option><?= $categories[$index]; ?></option>
@@ -21,22 +27,30 @@
             <span class="form__error">Выберите категорию</span>
         </div>
     </div>
-    <div class="form__item form__item--wide">
+    <div class="form__item form__item--wide <?php if ($errors['message']) {
+                                                print('form__item--invalid');
+                                            }; ?>">
         <label for="message">Описание</label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
-        <span class="form__error">Напишите описание лота</span>
+        <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
+        <span class="form__error"><?php
+                                    print($errors['lot-name']);
+                                    ?></span>
     </div>
-    <div class="form__item form__item--file">
+    <div class="form__item form__item--file <?php if ($errors['image']) {
+                                                print('form__item--invalid');
+                                            } else {
+                                                print('form__item--uploaded');
+                                            } ?>">
         <!-- form__item--uploaded -->
-        <label>Изображение</label>
+        <label>Изображение <?php print($path); ?></label>
         <div class="preview">
             <button class="preview__remove" type="button">x</button>
             <div class="preview__img">
-                <img src="img/avatar.jpg" width="113" height="113" alt="Изображение лота">
+                <img src="img/" width="113" height="113" alt="Изображение лота">
             </div>
         </div>
         <div class="form__input-file">
-            <input class="visually-hidden" type="file" id="photo2" value="">
+            <input class="visually-hidden" type="file" id="photo2" name="lot-image" value="">
             <label for="photo2">
                 <span>+ Добавить</span>
             </label>
@@ -45,17 +59,17 @@
     <div class="form__container-three">
         <div class="form__item form__item--small">
             <label for="lot-rate">Начальная цена</label>
-            <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
+            <input id="lot-rate" type="number" name="lot-rate" placeholder="0">
             <span class="form__error">Введите начальную цену</span>
         </div>
         <div class="form__item form__item--small">
             <label for="lot-step">Шаг ставки</label>
-            <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
+            <input id="lot-step" type="number" name="lot-step" placeholder="0">
             <span class="form__error">Введите шаг ставки</span>
         </div>
         <div class="form__item">
             <label for="lot-date">Дата окончания торгов</label>
-            <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
+            <input class="form__input-date" id="lot-date" type="date" name="lot-date">
             <span class="form__error">Введите дату завершения торгов</span>
         </div>
     </div>
